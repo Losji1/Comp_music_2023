7ba75ec7abe163795f9996844f7cfde18fc591db
Losji1 <julianvanpol@gmail.com>
2023-03-31 14:08:21 +0200

    Added the code for this project

---
+++ Code_project.R
@@ -0,0 +1,21 @@
+library(tidyr)
+library(remotes)
+library(spotifyr)
+library(usethis)
+library(ggplot2)
+library(dplyr)
+
+all_out_2010 <- get_playlist_audio_features("", "37i9dQZF1DX5Ejj0EkURtP")
+all_out_2000 <- get_playlist_audio_features("", "37i9dQZF1DX4o1oenSJRJd")
+
+ggplot() +
+  geom_density(data = all_out_2010, aes(x=tempo, fill="All Out 2010"), alpha=0.5) +
+  geom_density(data = all_out_2000, aes(x=tempo, fill="All Out 2000"), alpha=0.5) +
+  labs(title="Tempo Distribution", x="Tempo", y="Density") +
+  scale_fill_manual(values=c("blue", "green"))
+
+ggplot() +
+  geom_point(data = all_out_2010, aes(x=energy, y=valence, color="All Out 2010"), alpha=0.5) +
+  geom_point(data = all_out_2000, aes(x=energy, y=valence, color="All Out 2000"), alpha=0.5) +
+  labs(title="Energy vs. Valence", x="Energy", y="Valence") +
+  scale_color_manual(values=c("blue", "green"))
